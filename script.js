183daf24bf58eac466a34dfb2aef332a7e34b08b
lujolfs <lucas.j.frank@gmail.com>
2022-08-22 15:43:50 -0300

    Implementação da virada das cartas

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 var numero = 0;
+const imagens = ['imagens/carta1.gif', 'imagens/carta2.gif', 'imagens/carta3.gif', 'imagens/carta4.gif', 'imagens/carta5.gif', 'imagens/carta6.gif', 'imagens/carta7.gif'];
+let conVirada = 0;
+let cartaClicada = 0;
+let contagemErro = 0;
+let pontos = 0;
 
 distriCartas(prompt("Com quartas cartas deseja jogar? Insira um número par de 4 a 14!"));
 
@@ -7,10 +12,25 @@ function distriCartas(numero) {
         numero = prompt("Por favor, escolha um número par de 4 a 14.");
     }
 
-    const tabuleiro = document.querySelector(".tabuleiro");
+    const tabuleiro = document.querySelector(".jogo-da-memoria");
     tabuleiro.innerHTML = "";
 
     for (let i = 0; i < numero; i++) {
-        tabuleiro.innerHTML += "<li class=capa><img src='imagens/capa.png'></li>"
+        tabuleiro.innerHTML += "<div class=carta-memoria onclick='virarCarta(this)'><img class='frente' src='imagens/capa.png'><img class='costas' src='imagens/carta1.gif'></div>";
     }
 }
+
+function virarCarta(carta) {
+    if (conVirada == 0) {
+        carta.classList.add('virada');
+        conVirada++;
+    } else if (conVirada == 1 && carta.classList.contains('virada')) {
+        alert("Escolha uma carta diferente!");
+    } else if (conVirada == 1) {
+        carta.classList.add('virada-1');
+        conVirada++;
+    }
+}
+
+
+
